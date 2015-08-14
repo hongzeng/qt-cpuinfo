bc1e4aa3285e2bbeffdba17df29aacd01e125f89
Hong Zeng <hongzeng@hotmail.com>
2015-08-14 07:28:16 -0400

    use QFile

--- qt-cpuinfo.c
+++ qt-cpuinfo.c
@@ -1,7 +1,5 @@
 /* This is home work
  * code not verified with compiler as I didn't have Qt installed.
- * buffer allocation size was estimated for simplifying work,
- * suppose it big enough for /proc/cpuinfo.
  * window size was not verified
  */
 
@@ -11,31 +9,18 @@
 int main(int argc, char* argv[]) {
     QApplication myApp(argc, argv);
     QWidget* myWidget = new QWidget();
-    myWidget->setGeometry(400, 300, 120, 90);
+    myWidget->setGeometry(300, 300, 150, 190);
 
-    int input_fd;
-    int ret_in;
-    input_fd = open(“/proc/cpuinfo”, O_RDONLY);
-    if(input_fd == -1) {
-        perror (“open”);
-        return 2;
-    }
-
-    char buffer [BUF_SIZE*128]={0}; //simplified here, just allocate big enough buffer to
-                                    //read in all text from /proc/cpuinfo
-    char * BufferP = buffer;
-    strcpy(buffer, “CPU info:\n”);  //title of info
-    BufferP += strlen(buffer);      //increment the size of “CPU info:\n”
-
-    while((ret_in = read(input_fd, &BufferP, BUF_SIZE))>0){//suppose the buffer big enough
-        BufferP += BUF_SIZE;
-    }
-
-    close (input_fd);
+    QFile input_fd("/proc/cpuinfo");
+    if(!input_fd.open(QIODevice::ReadOnly | QIODevice::Text))
+	return -1;
+    QTextStream in(&input_fd);
     QString cpuInfoDisplay;
-    cpuInfoDisplay = buffer;
+    cpuInfoDisplay = in.readAll();
+    input_fd.close();
+
     QLabel *myLabel = new QLabel(cpuInfoDisplay,myWidget); //display it
-    myLabel->setGeometry(10, 10, 80, 30);
+    myLabel->setGeometry(10, 10, 120, 150);
     QPushButton* myQuitButton = new QPushButton("Quit", myWidget);
     myQuitButton->setGeometry(10, 50, 100, 30);
     QObject::connect(myQuitButton, SIGNAL(clicked()), &myApp, SLOT(quit());
